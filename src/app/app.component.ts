055f0441bd3abfe9762e32caddab14919aa14754
Ayan Kumar Saha <ayan.saha@ingredion.com>
2021-11-29 16:14:14 +0530

    Add event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,40 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BpmnConstantsService } from './bpmn-constants.service';
 import { BpmnService } from './bpmn.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
-  @ViewChild('jsDropZone', { static: true }) private el: ElementRef;;
+  @ViewChild('jsDropZone', { static: true }) private el: ElementRef;
   @ViewChild('canvas', { static: true }) private canvas: ElementRef;
-  
+
   constructor(public bpmnService: BpmnService) {}
 
   create() {
-    this.bpmnService.createNewDiagram(this.el);
+    this.bpmnService.createNewDiagram(
+      BpmnConstantsService.NEW_DIAGRAM_XML,
+      this.el
+    );
+  }
+
+  async ngOnInit() {
+    const diagram = localStorage.getItem('diagram');
+    await this.bpmnService.createNewDiagram(diagram, this.el);
+
+    this.bpmnService?.eventOutput?.subscribe(res => {
+      console.log('CLICKED 🚀', res);
+    })
   }
 
   ngAfterContentInit() {
-    this.bpmnService.attachModeler(this.canvas);
+    this.bpmnService.attachModelerToCanvas(this.canvas);
   }
 
+  async save() {
+    const xml = await this.bpmnService.exportDiagram();
+    localStorage.setItem('diagram', xml);
+  }
 }
--- src/app/bpmn.service.ts
+++ src/app/bpmn.service.ts
@@ -1,61 +1,64 @@
-import { ElementRef, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import {
+  ElementRef,
+  Injectable,
+  Renderer2,
+  RendererFactory2,
+} from '@angular/core';
 import * as BpmnModeler from 'bpmn-js/dist/bpmn-modeler.production.min.js';
+import { of, Subject } from 'rxjs';
+import { BpmnConstantsService } from './bpmn-constants.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BpmnService {
-  private readonly NEW_DIAGRAM_XML = `
-  <?xml version="1.0" encoding="UTF-8"?>
-  <bpmn2:definitions xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn">
-   <bpmn2:process id="Process_1" isExecutable="false">
-      <bpmn2:startEvent id="StartEvent_1" />
-   </bpmn2:process>
-   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-      <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
-         <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
-            <dc:Bounds height="36.0" width="36.0" x="412.0" y="240.0" />
-         </bpmndi:BPMNShape>
-      </bpmndi:BPMNPlane>
-   </bpmndi:BPMNDiagram>
-  </bpmn2:definitions>;`
-  
-  private bpmnModeler;
-  private renderer: Renderer2;
 
-  constructor(
-    private rendererFactory: RendererFactory2
-  ) {
-    this.bpmnModeler = new BpmnModeler();
-    this.renderer = rendererFactory.createRenderer(null, null);
+  private _bpmnModeler: any;
+  private _renderer: Renderer2 = null;
+
+  public eventOutput: Subject<null | string> = new Subject<null>();
+
+  constructor(private _rendererFactory: RendererFactory2) {
+    this._bpmnModeler = new BpmnModeler();
+    this._renderer = _rendererFactory.createRenderer(null, null);
   }
 
-  public async createNewDiagram(el: ElementRef): Promise<void> {
-    await this.openDiagram(this.NEW_DIAGRAM_XML, el);
+  public async createNewDiagram(xml: string, el: ElementRef): Promise<void> {
+    const diagramXML = xml ? xml : BpmnConstantsService.NEW_DIAGRAM_XML;
+    await this.openDiagram(diagramXML, el);
   }
 
-  public async openDiagram(xml: string, el: ElementRef) {
+  public async openDiagram(xml: string, el: ElementRef): Promise<void> {
     try {
-      await this.bpmnModeler.importXML(xml);
-      
-      this.renderer.addClass(el.nativeElement, 'with-diagram');
-      this.renderer.removeClass(el.nativeElement, 'with-error');
+      await this._bpmnModeler.importXML(xml);
 
+      this._renderer.addClass(el.nativeElement, 'with-diagram');
+      this._renderer.removeClass(el.nativeElement, 'with-error');
+
+      var eventBus = this._bpmnModeler.get('eventBus');
+
+      eventBus.on('element.dblclick', (e) => { 
+        this.eventOutput.next(e.element.id)
+      })
+      
     } catch (err) {
-      this.renderer.removeClass(el.nativeElement, 'with-diagram');
-      this.renderer.addClass(el.nativeElement, 'with-error');
+      this._renderer.removeClass(el.nativeElement, 'with-diagram');
+      this._renderer.addClass(el.nativeElement, 'with-error');
     }
   }
 
-  public attachModeler(el: ElementRef) {
-    this.bpmnModeler.attachTo(el.nativeElement);
+  public attachModelerToCanvas(el: ElementRef): void {
+    this._bpmnModeler.attachTo(el.nativeElement);
   }
 
   public closeDiagram() {
-    this.bpmnModeler.destroy();
+    this._bpmnModeler.destroy();
   }
 
-  public saveDiagram() {
+  public async exportDiagram(): Promise<string> {
+    const result = await this._bpmnModeler.saveXML();
+    const { xml } = result;
     
+    return xml;
   }
 }
